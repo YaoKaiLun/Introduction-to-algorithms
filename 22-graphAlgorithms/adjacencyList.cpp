ee5a2e9a597b7947ee65fed7e79571e136ee1e1b
allen <yaokailun@ifanr.com>
2017-11-12 22:13:41 +0800

    prettify code

--- 22-graphAlgorithms/adjacencyList.cpp
+++ 22-graphAlgorithms/adjacencyList.cpp
@@ -8,26 +8,30 @@ struct Node {
 
 class Graph {
   int verticeAmount;
-  list<Node> * adjLists;
+  list<Node> *adjLists;
 public:
-  Graph(int amount);
+  Graph(const int amount, Node *nodes);
   void addEdge(Node start, Node end);
   void printGraph();
 };
 
-Graph::Graph(int amount) {
+Graph::Graph(const int amount, Node *nodes) {
   verticeAmount = amount;
   adjLists = new list<Node>[verticeAmount];
+  for(int i = 0; i < verticeAmount; i++) {
+    adjLists[i].push_back(nodes[i]);
+  }
 };
 
 void Graph::addEdge(Node start, Node end) {
-  adjLists[start.key].push_front(end);
+  adjLists[start.key].push_back(end);
 };
 
 void Graph::printGraph() {
   for(int i = 0; i < verticeAmount; i++) {
-    printf("%d", i);
-    for(list<Node>::iterator j = adjLists[i].begin(); j != adjLists[i].end(); j++) {
+    list<Node>::iterator j = adjLists[i].begin();
+    printf("%d", j->key);
+    for(++j; j != adjLists[i].end(); j++) {
       printf(" -> %d", j->key);
     }
     printf("\n");
@@ -35,15 +39,22 @@ void Graph::printGraph() {
 };
 
 int main(int argc, char *argv[]) {
-  int verticeAmount = 4;
+  const int verticeAmount = 4;
+
   Node node0 = {0};
   Node node1 = {1};
   Node node2 = {2};
   Node node3 = {3};
-  Graph graph(verticeAmount);
+
+  Node nodes[verticeAmount] = {node0, node1, node2, node3};
+
+  Graph graph(verticeAmount, nodes);
+
   graph.addEdge(node0, node1);
   graph.addEdge(node0, node2);
   graph.addEdge(node1, node2);
   graph.addEdge(node2, node3);
+
+  printf("the graph is: \n");
   graph.printGraph();
 };
