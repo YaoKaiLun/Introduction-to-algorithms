81134f979d9136fc9f3ba0bfdbaf6e163074d22a
allen <yaokailun@ifanr.com>
2017-12-03 13:02:35 +0800

    adjust the code struct of  bellman ford algorithm

--- 24-singleSourceShortestPaths/Node.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef Node_hpp
-#define Node_hpp
-
-struct Node {
-  int distance;
-  int parent;
-};
-
-#endif
--- 24-singleSourceShortestPaths/bellmanFord.cpp
+++ 24-singleSourceShortestPaths/bellmanFord.cpp
@@ -1,7 +1,35 @@
-#include "Graph.hpp"
-#include "Edge.hpp"
+#include <vector>
+
+using namespace std;
 
 const int MAX_VALUE = 100000;
+
+struct Edge {
+  int src;
+  int dest;
+  int weight;
+};
+
+struct Node {
+  int distance;
+  int parent;
+};
+
+class Graph {
+  protected:
+    int vertexAmount;
+    int edgeAmount;
+    Node *nodes;
+    vector <Edge> edges;
+    void init(int start);
+    int weight(int src, int dest);
+    void relax(int src, int dest);
+    void printNodes();
+  public:
+    Graph(int vertexAmount, int edgeAmount);
+    void addEdge(int src, int dest, int weight);
+    void bellmanFord(int start);
+};
 
 Graph::Graph(int vAmount, int eAmount) {
   vertexAmount = vAmount;
@@ -58,4 +86,17 @@ void Graph::bellmanFord(int start) {
     // printNodes();
   }
   printNodes();
+}
+
+int main(int argv, char *argc[]) {
+  Graph *graph = new Graph(5, 8);
+  graph->addEdge(0, 1, -1);
+  graph->addEdge(0, 2, 4);
+  graph->addEdge(1, 2, 3);
+  graph->addEdge(1, 3, 2);
+  graph->addEdge(1, 4, 2);
+  graph->addEdge(3, 2, 5);
+  graph->addEdge(3, 1, 1);
+  graph->addEdge(4, 3, -3);
+  graph->bellmanFord(0);
 }
